54c747612d19a50863c2b0f4ad0f6a700a304840
maxvoitenko <maxim.voitenko@planetvpn.dev>
2024-01-21 22:32:32 +0200

    fix swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,24 +1,39 @@
-import { INestApplication } from '@nestjs/common';
-import {
-  DocumentBuilder,
-  SwaggerDocumentOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
+import {INestApplication} from '@nestjs/common';
+import {DocumentBuilder, SwaggerDocumentOptions, SwaggerModule,} from '@nestjs/swagger';
 
-export function setupSwagger(app: INestApplication) {
-  const option: SwaggerDocumentOptions = {
-    ignoreGlobalPrefix: false,
-  };
-  const config = new DocumentBuilder()
-    .setTitle('Grippo API')
-    .setDescription(`Desc`)
-    .setVersion('1.0')
-    .addBearerAuth();
+export function setupSwagger(app: INestApplication): void {
+    const options: SwaggerDocumentOptions = {
+        ignoreGlobalPrefix: false,
+    };
 
-  const document = SwaggerModule.createDocument(app, config.build(), option);
+    const config = new DocumentBuilder()
+        .setTitle('🏋️ Grippo API')
+        .setDescription('📘 Full documentation for Grippo API endpoints.')
+        .setVersion('1.0.0')
+        .addBearerAuth(
+            {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+                name: 'Authorization',
+                description: 'Enter JWT token',
+                in: 'header',
+            },
+            'access-token'
+        )
+        .build();
 
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    }});
+    const document = SwaggerModule.createDocument(app, config, options);
+
+    SwaggerModule.setup('docs', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+            docExpansion: 'none', // 'list' | 'full' | 'none'
+            displayRequestDuration: true,
+            deepLinking: true,
+            tagsSorter: 'alpha',
+            operationsSorter: 'alpha',
+        },
+        customSiteTitle: 'Grippo API Docs',
+    });
 }
